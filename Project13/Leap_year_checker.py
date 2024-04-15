d586bf36ea2ffba150f523431a71324b8660808f
Israr_dawar <77443384+israr96418@users.noreply.github.com>
2024-04-15 11:01:11 +0500

    Update Leap_year_checker.py

--- Project13/Leap_year_checker.py
+++ Project13/Leap_year_checker.py
@@ -1,17 +1,21 @@
-def Leap_year_checker(year):
-    if year % 4 == 0:
-        if year % 100 == 0:
-            if year % 400 == 0:
-                print("The year is leap year")
-            else:
-                print("The year is not a leap year")
-        else:
-            print("The Year is a leap year")
+def is_leap_year(year):
+     """
+     Check if a given year is a leap year.
+    
+    Args:
+        year (int): The year to be checked.
+    
+    Returns:
+        bool: True if the year is a leap year, False otherwise.
+    """
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        return True
     else:
-        print("The year is not a leap year")
-
-
+        return False
 
 if __name__ == '__main__':
-    year = int(input('Enter year that you want to check whether it is leap year or not: '))
-    Leap_year_checker(year)
+    year = int(input('Enter the year that you want to check whether it is a leap year or not: '))
+    if is_leap_year(year):
+        print(f"{year} is a leap year.")
+    else:
+        print(f"{year} is not a leap year.")
